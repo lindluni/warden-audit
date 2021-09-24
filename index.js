791c1c68721159eecf89f975d425f05a01de2982
Brett Logan <lindluni@github.com>
2021-09-24 01:10:07 -0400

    Add logging

--- index.js
+++ index.js
@@ -7,74 +7,108 @@ const {enterpriseServer30Admin} = require("@octokit/plugin-enterprise-server");
 const _Octokit = Octokit.plugin(enterpriseServer30Admin, retry, throttling);
 
 (async function main() {
-    const token = core.getInput('TOKEN', {required: true, trimWhitespace: true})
-    const url = core.getInput('URL', {required: true, trimWhitespace: true})
-    const user = core.getInput('USER', {required: true, trimWhitespace: true})
+    try {
+        const token = core.getInput('TOKEN', {required: true, trimWhitespace: true})
+        const url = core.getInput('URL', {required: true, trimWhitespace: true})
+        const user = core.getInput('USER', {required: true, trimWhitespace: true})
 
-    let client = new _Octokit({
-        auth: token,
-        baseUrl: url,
-        throttle: {
-            onRateLimit: (retryAfter, options, octokit) => {
-                octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`);
-                if (options.request.retryCount === 0) {
-                    octokit.log.info(`Retrying after ${retryAfter} seconds!`);
-                    return true;
+        let client
+        try {
+            client = new _Octokit({
+                auth: token,
+                baseUrl: url,
+                throttle: {
+                    onRateLimit: (retryAfter, options, octokit) => {
+                        octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`);
+                        if (options.request.retryCount === 0) {
+                            octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+                            return true;
+                        }
+                    },
+                    onAbuseLimit: (retryAfter, options, octokit) => {
+                        octokit.log.warn(`Abuse detected for request ${options.method} ${options.url}`);
+                    },
                 }
-            },
-            onAbuseLimit: (retryAfter, options, octokit) => {
-                octokit.log.warn(`Abuse detected for request ${options.method} ${options.url}`);
-            },
+            })
+        } catch (e) {
+            fail(`Failed creating client: ${e}`)
         }
 
-    })
-
-    core.info(`Creating impersonation token for user: ${user}`)
-    const impersonationToken = await client.request('POST /admin/users/{username}/authorizations', {
-        username: user,
-        scopes: [
-            'repo'
-        ]
-    })
-    client = new _Octokit({
-        auth: impersonationToken.data.token,
-        baseUrl: url,
-        throttle: {
-            onRateLimit: (retryAfter, options, octokit) => {
-                octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`);
-                if (options.request.retryCount === 0) {
-                    octokit.log.info(`Retrying after ${retryAfter} seconds!`);
-                    return true;
-                }
-            },
-            onAbuseLimit: (retryAfter, options, octokit) => {
-                octokit.log.warn(`Abuse detected for request ${options.method} ${options.url}`);
-            },
+        let impersonationToken
+        try {
+            core.info(`Creating impersonation token for user: ${user}`)
+            impersonationToken = await client.request('POST /admin/users/{username}/authorizations', {
+                username: user,
+                scopes: [
+                    'repo'
+                ]
+            })
+        } catch (e) {
+            fail(`Failed generating impersonation token: ${e}`)
         }
-    })
-    const access = {
-        orgs: [],
-        repos: []
-    }
 
-    core.info(`Fetch orgs for user`)
-    const orgs = await client.paginate(client.orgs.listForAuthenticatedUser, {
-        per_page: 100
-    })
-    for (const org of orgs) {
-        access.orgs.push(org.login)
-    }
+        try {
+            client = new _Octokit({
+                auth: impersonationToken.data.token,
+                baseUrl: url,
+                throttle: {
+                    onRateLimit: (retryAfter, options, octokit) => {
+                        octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`);
+                        if (options.request.retryCount === 0) {
+                            octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+                            return true;
+                        }
+                    },
+                    onAbuseLimit: (retryAfter, options, octokit) => {
+                        octokit.log.warn(`Abuse detected for request ${options.method} ${options.url}`);
+                    },
+                }
+            })
+        } catch (e) {
+            fail(`Failed creating client: ${e}`)
+        }
 
-    core.info(`Fetch repos for user`)
-    const repos = await client.paginate(client.repos.listForAuthenticatedUser, {
-        per_page: 100
-    })
-    for (const repo of repos) {
-        access.repos.push({
-            name: repo.full_name,
-            permission: repo.permissions
-        })
+        const access = {
+            orgs: [],
+            repos: []
+        }
+
+        let orgs
+        try {
+            core.info(`Fetch orgs for user`)
+            orgs = await client.paginate(client.orgs.listForAuthenticatedUser, {
+                per_page: 100
+            })
+            for (const org of orgs) {
+                access.orgs.push(org.login)
+            }
+        } catch (e) {
+            fail(`Failed fetching user orgs: ${e}`)
+        }
+
+        let repos
+        try {
+            core.info(`Fetch repos for user`)
+            repos = await client.paginate(client.repos.listForAuthenticatedUser, {
+                per_page: 100
+            })
+            for (const repo of repos) {
+                access.repos.push({
+                    name: repo.full_name,
+                    permission: repo.permissions
+                })
+            }
+        } catch (e) {
+            fail(`Failed fetching repos: ${e}`)
+        }
+        core.info('Audit Log:')
+        core.info(JSON.stringify(access))
+    } catch (e) {
+        core.error(`Unable to retrieve input: ${e}`)
     }
-    core.info('Audit Log:')
-    core.info(JSON.stringify(access))
 })()
+
+function fail(error) {
+    core.setFailed(error)
+    process.exit(1)
+}
