27d023bb5b23c0e72b80a16741e7dce3df4984ff
Brett Logan <lindluni@github.com>
2021-09-24 00:58:08 -0400

    Debug

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const _Octokit = Octokit.plugin(enterpriseServer30Admin, retry, throttling);
 
     console.log(process.env)
     const impersonationToken = await client.request('POST /admin/users/{username}/authorizations', {
-        username: process.env.USER,
+        username: process.env.GHES_USER,
         scopes: [
             'repo'
         ]
